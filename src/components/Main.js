13f6b30647420b6efecdec060d6cce1aac6e2b96
Farul Ivan <farulivan@gmail.com>
2022-08-07 21:42:45 +0700

    making light mode

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,16 @@
 import React from "react";
-import ReactLogo2 from "../images/reactjs-icon2.png"
 
-export default function Main() {
+export default function Main(props) {
     return (
-        <section className="main-text">
-            <h1>Fun facts about React</h1>
-            <ul>
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </section>
+        </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactLogo from "../images/reactjs-icon.png"
 
-export default function Navbar() {
+export default function Navbar(props) {
+    console.log(props.darkMode)
+    console.log(props.toggleDarkMode)
     return (
-        <nav>
-            <img src={ReactLogo} alt="react logo" />
-            <h2>ReactFacts</h2>
-            <h3>React Course - Project 1</h3>
+        <nav className={props.darkMode ? "dark": ""}>
+            <div className="nav--logo">
+                <img className="nav--logo_icon" src={ReactLogo} alt="react logo" />
+                <div className="nav--logo_text">
+                    <h2>ReactFacts</h2>
+                    <h3>React Course - Project 1</h3>
+                </div>
+            </div>
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div className="toggler--slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
         </nav>
     )
 }
